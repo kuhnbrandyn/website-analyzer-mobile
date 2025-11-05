bcc2d6f0c750def11747dad48e53b77d0832005e
kuhnbrandyn <kuhn.brandyn@gmail.com>
2025-11-04 20:03:49 -0500

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AnalyzerScreen from "./screens/AnalyzerScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -9,11 +10,12 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Analyzer"
+        initialRouteName="Login"
         screenOptions={{
           headerShown: false,
         }}
       >
+        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Analyzer" component={AnalyzerScreen} />
       </Stack.Navigator>
     </NavigationContainer>
